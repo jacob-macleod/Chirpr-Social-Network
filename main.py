062004287ae5fd0712ddcb8da0cb9dbe884be0bb
jacob-macleod <jacob.e.macleod@gmail.com>
2021-03-06 21:27:07 +0000

    Added login & post system

---
+++ main.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
+import pymongo
+import json
+
+app = Flask(__name__)
+global username
+username = ""
+
+#Initialise mongodb
+client = pymongo.MongoClient()
+database = client["social_app_db"]
+
+#Add a posts collection
+posts = database["posts"]
+login_data = database["login_data"]
+
+
+@app.route("/")
+def index():
+    global username
+
+    post_contents = request.cookies.get("post")
+    #If the user is not logged in
+    if request.cookies.get("username") == None or request.cookies.get("username") == "" and username == "":
+        return render_template("login.html", username=username)
+    else :
+        return render_template("index.html", username=username)
+
+    if post_contents != "%NoneValue%" :
+        post_data = {"username":"Jacob", "post":post_contents}
+        posts.insert(post_data)
+    return render_template("index.html", username=username)
+
+
+
+@app.route("/sign_up", methods=["POST", "GET"])
+def sign_up () :
+    global username
+    if request.method == "POST":
+        username = request.form.get("uname") 
+        password = request.form.get("pword") 
+        password_confirm = request.form.get("pword_confirm") 
+
+        #Check username already exists
+        try :
+            if login_data.find_one({"username":{"$eq":username}}).get("username") != "" :
+                user_exists = True
+        except:
+            user_exists = False
+
+        #Check if password and confirmed passwords match and if user already exists, and if they do, direct to an error page
+        if password != password_confirm or user_exists == True:
+            username = ""
+            return render_template("login_failed.html")
+        else :
+            login = {"username":username, "password":password}
+            login_data.insert_one(login)
+            return render_template("redirect_to_index.html")
+    return render_template("sign_up.html", username=username)
+
+
+@app.route("/sign_in", methods=["POST", "GET"])
+def sign_in () :
+    global username
+
+    #If user clicked sign in
+    if request.method == "POST" and request.form.get("uname") != None:
+        username = request.form.get("uname") 
+        password = request.form.get("pword") 
+
+        #See if the username and password match the one in the database and display an error page or redirect to the main page accordingly
+        try :
+            if login_data.find_one({"username":{"$eq":username}}).get("username") != "" and login_data.find_one({"password":{"$eq":password}}).get("username") != "" :
+                username = username
+                return render_template("redirect_to_index.html")
+        except :
+            username = ""
+            return render_template("signin_failed.html")
+    return render_template("sign_in.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5001)
